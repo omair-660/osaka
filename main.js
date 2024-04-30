39756c6614e46be235f5397173e0c7363ede5cd1
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-05-01 00:38:47 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-// Function to start the counting animation
 function startCountingAnimation() {
     const counters = document.querySelectorAll('.counter');
     
-    // Options for the intersection observer
+    
     const options = {
-      root: null, // Use the viewport as the root
-      threshold: 0.75 // Trigger the animation when 75% of the element is visible
+      root: null, 
+      threshold: 0.75 
     };
     
-    // Intersection observer callback function
+    
     const callback = function(entries, observer) {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           const target = +entry.target.getAttribute('data-target');
-          const increment = Math.ceil(target / 100); // Increment by 1% of the target value
+          const increment = Math.ceil(target / 100); 
           let currentCount = 0;
           
-          // Function to increment the count
+  
           const updateCount = () => {
             if (currentCount < target) {
               currentCount += increment;
               entry.target.textContent = currentCount;
-              setTimeout(updateCount, 10); // Adjust the speed of the animation here
+              setTimeout(updateCount, 10);
             } else {
               entry.target.textContent = target;
               observer.unobserve(entry.target);
@@ -33,15 +32,21 @@ function startCountingAnimation() {
       });
     };
     
-    // Create an intersection observer
+    
     const observer = new IntersectionObserver(callback, options);
     
-    // Observe each counter element
     counters.forEach(counter => {
       observer.observe(counter);
     });
   }
   
-  // Call the function to start the counting animation when the DOM is loaded
   document.addEventListener('DOMContentLoaded', startCountingAnimation);
-  
+
+  let span2 = document.getElementById("span-2");
+  let span3 = document.getElementById("span-3");
+  let menu = document.getElementsByClassName("navbar-toggler")[0];
+
+  menu.onclick = function(){
+    span2.classList.toggle("open");
+    span3.classList.toggle("open");
+  }
